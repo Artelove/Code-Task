29ba8f791ee21fd6c3932cb1ecb33bb5a91a0842
Artelove <arteevil.off@gmail.com>
2022-07-17 09:19:34 +0300

    629. K Inverse Pairs Array

---
+++ LeetCode/C#/Hard/629. K Inverse Pairs Array/629. K Inverse Pairs Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    private int[] array;
+    private int[,] cache;
+    static void Main()
+    {
+        int n = 3;
+        int k = 1;
+        Program program = new Program();
+        Console.WriteLine(program.KInversePairs(n, k));
+    }
+
+    public int KInversePairs(int n, int k)
+    {
+        int[,] dp = new int[n + 1, k + 1];
+        int countInversePairs = 0;
+        int MOD = 1000000007;
+        dp[0, 0] = 1;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            
+            for (int j = 0; j < k + 1; j++)
+            {
+                if (j == 0)
+                {
+                    dp[i, j] = 1;
+                    countInversePairs = 1;
+                }
+                else
+                {
+                    countInversePairs = (countInversePairs + dp[i - 1, j]) % MOD;
+                    if (j - i >= 0)
+                    {
+                        countInversePairs = (countInversePairs + MOD - dp[i - 1, j - i]) % MOD;
+                    }
+                    dp[i, j] = countInversePairs;
+                }
+            }
+        }
+
+        return dp[n, k];
+    }
+}
