3ccc2a7b002a30a8f8ede99e44600316a5eaf6e1
Artelove <arteevil.off@gmail.com>
2022-07-20 00:28:53 +0300

    1. Two Sum

---
+++ LeetCode/C#/Easy/1. Two Sum/1. Two Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = {0,0,2,7};
+        int target = 9;
+        Console.WriteLine(solution.TwoSum(nums, target)[0] + " " + solution.TwoSum(nums, target)[1]);
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = 0; j < nums.Length; j++)
+                if (i != j && nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+        return new int[0];
+    }
+}
