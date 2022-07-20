7da66824636f40d70d25ea3da280ac61f33e5df7
Artelove <arteevil.off@gmail.com>
2022-07-20 09:18:20 +0300

    792. Number of Matching Subsequences (Dictionary).

---
+++ LeetCode/C#/2. Medium/792. Number of Matching Subsequences (Dictionary)/792. Number of Matching Subsequences (Dictionary)/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "dsahjpjauf";
+        string[] words = new string[] { "ahjpjau", "ja", "ahbwzgqnuk", "tnmlanowax" };
+        Console.WriteLine(solution.NumMatchingSubseq(s, words));
+    }
+    public int NumMatchingSubseq(string s, string[] words)
+    {
+        int count = 0;
+        var wordCount = new Dictionary<string, int>();
+        var isSubSequenceConfirmed = new Dictionary<string, bool>();
+
+        foreach (var word in words)
+        {
+            if (!wordCount.ContainsKey(word))
+                wordCount[word] = 0;
+            wordCount[word]++;
+            isSubSequenceConfirmed[word] = false;
+        }
+
+        foreach (var word in wordCount.Keys)
+        {
+            if(isSubSequenceConfirmed[word]==true)
+                count += wordCount[word];
+            if (IsSubSequence(s, word))
+            {
+                isSubSequenceConfirmed[word] = true;
+                count += wordCount[word];
+            }
+        }
+
+        return count;
+    }
+
+    private bool IsSubSequence(string srt, string word)
+    {
+        int i = 0, j = 0;
+        while (i < srt.Length && j < word.Length)
+            if (srt[i++] == word[j])
+                j++;
+        return j == word.Length;
+    }
+}
