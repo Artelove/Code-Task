ba07bf027e9ab0a01fc736a91bc8d4c818df246b
Artelove <arteevil.off@gmail.com>
2022-07-20 09:00:23 +0300

    792. Number of Matching Subsequences &  Folders name
    fix(folder): Change folders name to correct order

---
+++ LeetCode/C#/1. Easy/14. Longest Common Prefix/14. Longest Common Prefix/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string[] strs = new string[] { "a" };
+        Console.WriteLine(solution.LongestCommonPrefix(strs));
+    }
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int min = strs[0].Length;
+        string longestCommonPrefix = "";
+        char currentChar = ' ';
+        foreach (string str in strs)
+            if (str.Length < min)
+                min = str.Length;
+
+        for (int i = 0; i < min; i++)
+        {
+            currentChar = strs[0][i];
+            foreach (string str in strs)
+            {
+                if (str[i] != currentChar)
+                    return longestCommonPrefix;
+            }
+            longestCommonPrefix += currentChar;
+        }
+        return longestCommonPrefix;
+    }
+}
---
+++ LeetCode/C#/2. Medium/792. Number of Matching Subsequences/792. Number of Matching Subsequences/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "dsahjpjauf";
+        string[] words = new string[] { "ahjpjau","ja","ahbwzgqnuk","tnmlanowax" };
+        Console.WriteLine(solution.NumMatchingSubseq(s, words));
+    }
+    public int NumMatchingSubseq(string s, string[] words)
+    {
+        bool isSubsequence;
+        bool canContinue;
+        int countSubsequence = 0;
+        foreach (string word in words)
+        {
+            isSubsequence = true;
+            int n = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                canContinue = false;
+                for (int j = n; j < s.Length; j++)
+                {
+                    if (word[i] == s[j])
+                    {
+                        n = j + 1;
+                        canContinue = true;
+                        break;
+                    }
+                }
+                if (!canContinue)
+                {
+                    isSubsequence = false;
+                    break;
+                }
+            }
+            if (!isSubsequence) 
+                continue;
+            countSubsequence++;
+        }
+        return countSubsequence;
+    }
+}
