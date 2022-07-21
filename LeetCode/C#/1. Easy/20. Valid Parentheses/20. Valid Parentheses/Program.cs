bbc12bb30f8c893dd973b3f4dfbf596705fff372
Artelove <arteevil.off@gmail.com>
2022-07-21 11:12:00 +0300

    20. Valid Parentheses

---
+++ LeetCode/C#/1. Easy/20. Valid Parentheses/20. Valid Parentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string str = "()[[]][]";
+        Console.WriteLine(solution.IsValid(str));
+    }
+    public bool IsValid(string s)
+    {
+        List<char> needToClose = new List<char>();
+        foreach (char currentChar in s)
+        {
+            if ('(' == currentChar)
+                needToClose.Add(currentChar);
+            else
+            if ('{' == currentChar) needToClose.Add(currentChar);
+            else
+            if ('[' == currentChar) needToClose.Add(currentChar);
+            else
+            if (needToClose.Count > 0)
+            {
+                int arrCount = needToClose.Count - 1;
+                char needToCloseChar = needToClose[arrCount];
+                if ((')' == currentChar && '(' == needToCloseChar) ||
+                    ('}' == currentChar && '{' == needToCloseChar) ||
+                    (']' == currentChar && '[' == needToCloseChar))
+                        needToClose.RemoveAt(arrCount);
+                else
+                    return false;
+            }
+            else return false;
+        }
+        if (needToClose.Count == 0)
+            return true;
+        else
+            return false;
+
+    }
+}
