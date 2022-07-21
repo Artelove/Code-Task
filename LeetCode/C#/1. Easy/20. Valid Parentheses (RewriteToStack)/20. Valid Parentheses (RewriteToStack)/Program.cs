ecca2fcc02ed481ac6fd5a54e5eaf49d4c5b9b6c
Artelove <arteevil.off@gmail.com>
2022-07-21 12:20:32 +0300

    20. Valid Parentheses (RewriteToStack)

---
+++ LeetCode/C#/1. Easy/20. Valid Parentheses (RewriteToStack)/20. Valid Parentheses (RewriteToStack)/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string str = "{[]}()";
+        Console.WriteLine(solution.IsValid(str));
+    }
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (var item in s)
+        {
+            if (stack.Count > 0)
+            {
+                if (
+                    item == ')' && stack.Peek() == '(' ||
+                    item == ']' && stack.Peek() == '[' ||
+                    item == '}' && stack.Peek() == '{'
+                )
+                    stack.Pop();
+                else
+                    stack.Push(item);
+            }
+            else
+                stack.Push(item);
+        }
+
+        if (stack.Count == 0)
+            return true;
+        else
+            return false;
+    }
+}
