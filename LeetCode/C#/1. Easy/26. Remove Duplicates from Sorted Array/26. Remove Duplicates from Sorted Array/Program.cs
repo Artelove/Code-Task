663f0e0a6b0ac37240971cfc2896148b344dceb0
Artelove <arteevil.off@gmail.com>
2022-07-23 13:08:12 +0300

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode/C#/1. Easy/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = new int[] { 1,1,2,2,3,3,3,3,4,5,6,6,6,6};
+        Console.WriteLine(solution.RemoveDuplicates(nums));
+    }
+    public int RemoveDuplicates(int[] nums)
+    {
+        int currentDigit = nums[0];
+        int duplicatedCount = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > currentDigit)
+            {
+                currentDigit = nums[i];
+                nums[duplicatedCount] = nums[i];
+                duplicatedCount++;
+            }
+        }
+        return duplicatedCount;
+    }
+}
