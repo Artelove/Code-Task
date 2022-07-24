375b96e1b5730c8b64c8711cc20b7f43a7fac8f6
Artelove <arteevil.off@gmail.com>
2022-07-24 11:51:01 +0300

    35. Search Insert Position

---
+++ LeetCode/C#/1. Easy/35. Search Insert Position/35. Search Insert Position/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = new int[] { 1,3,5,6};
+        int target = 2;
+        Console.WriteLine(solution.SearchInsert(nums, target));
+    }
+    public int SearchInsert(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] >= target) 
+                return i;
+        }
+        return nums.Length;
+    }
+}
