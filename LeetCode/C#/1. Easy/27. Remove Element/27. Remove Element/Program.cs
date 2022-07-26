26ffdb03c0b334655b807886d092a24870e58200
Artelove <arteevil.off@gmail.com>
2022-07-26 23:16:20 +0300

    27. Remove Element

---
+++ LeetCode/C#/1. Easy/27. Remove Element/27. Remove Element/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = new int[] { 3,2,2,3 };
+        int val = 3;
+        Console.WriteLine(solution.RemoveElement(nums, val));
+    }
+    public int RemoveElement(int[] nums, int val)
+    {
+        int notValueIntCount = 0, i;
+        for (i = 0, notValueIntCount = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val) {
+                nums[notValueIntCount] = nums[i];
+                notValueIntCount++;
+            }
+        }
+        return notValueIntCount;
+    }
+}
