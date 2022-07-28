68484eaf11cc985a0112a367c7d944a41c6d46c0
Artelove <arteevil.off@gmail.com>
2022-07-28 10:26:50 +0300

    242. Valid Anagram (sorted list)

---
+++ LeetCode/C#/1. Easy/242. Valid Anagram (sorted list)/242. Valid Anagram (sorted array)/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "anagram";
+        string t = "nagaram";
+        Console.WriteLine(solution.IsAnagram(s,t));
+    }
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        List<char> listS = s.ToList();
+        List<char> listT = t.ToList();
+        listS.Sort();
+        listT.Sort();
+        if (listS.SequenceEqual(listT)) 
+            return true;
+        return false;
+    }
+}
