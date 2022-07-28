ff000fd5d7ff3dd59f8cfb666759c8bdd5840432
Artelove <arteevil.off@gmail.com>
2022-07-28 10:25:40 +0300

    242. Valid Anagram

---
+++ LeetCode/C#/1. Easy/242. Valid Anagram/242. Valid Anagram/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "a";
+        string t = "ab";
+        Console.WriteLine(solution.IsAnagram(s,t));
+    }
+    public bool IsAnagram(string s, string t)
+    {
+        if(s.Length != t.Length) return false; 
+        bool canCountinue;
+        List<char> listS = s.ToList();
+        List<char> listT = t.ToList();
+        while (listS.Count > 0)
+        {
+            foreach (char sChar in listS)
+            {
+                canCountinue = false;
+                foreach (char tChar in listT)
+                {
+                    if (sChar == tChar)
+                    {
+                        listS.Remove(sChar);
+                        listT.Remove(tChar);
+                        canCountinue = true;
+                        break;
+                    }
+                }
+                if (canCountinue) 
+                    break;
+                else 
+                    return false;
+            }
+        }
+        return true;
+    }
+}
