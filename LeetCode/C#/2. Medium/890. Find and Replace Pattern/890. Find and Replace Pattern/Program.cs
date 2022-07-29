6df348bb200c90370386b855afdb884e5f9b67d1
Artelove <arteevil.off@gmail.com>
2022-07-29 09:47:04 +0300

    890. Find and Replace Pattern

---
+++ LeetCode/C#/2. Medium/890. Find and Replace Pattern/890. Find and Replace Pattern/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string[] words = new string[] { "abc", "deq", "mee", "aqq", "dkd", "ccc" };
+        string pattern = "abb";
+        solution.FindAndReplacePattern(words, pattern);
+    }
+    string getPatternModel(string word)
+    {
+        char keyChar = 'a';
+        string patternModel = "";
+        Dictionary<char, char> keyValuePairs = new Dictionary<char, char>();
+        foreach (char c in word) { 
+            if (keyValuePairs.ContainsKey(c))
+                patternModel += keyValuePairs[c];
+            else
+            {
+                keyValuePairs.Add(c, keyChar++);
+                patternModel += keyValuePairs[c];
+            }
+        }
+        return patternModel;
+    }
+    public IList<string> FindAndReplacePattern(string[] words, string pattern)
+    {
+        IList<string> result = new List<string>();
+        string patternModel = getPatternModel(pattern);
+        foreach (var word in words)
+        {
+            if (patternModel == getPatternModel(word))
+                result.Add(word);
+        }
+        return result;
+    }
+}
