0a3ec3e5b6f04c26002062d792d2497abc69381a
Artelove <arteevil.off@gmail.com>
2022-08-06 22:45:21 +0300

    58. Length of Last Word (O(n))

---
+++ LeetCode/C#/1. Easy/58. Length of Last Word (O(n))/58. Length of Last Word (O(n))/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "luffy is still joyboy"
+        Console.WriteLine(solution.LengthOfLastWord(s));
+    }
+    public int LengthOfLastWord(string s)
+    {
+        int lenghtLastWord = 0;
+        int i, count = 0;
+        for (i = 0; i < s.Length; i++)
+            if (s[i] == ' ')
+            {
+                if(count!=0)
+                    lenghtLastWord = count;
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        if (count != 0)
+            lenghtLastWord = count;
+        return lenghtLastWord;
+    }
+}
