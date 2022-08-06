b1b6df722e7e65969bba7ec6c8b2fcbb4cfc4ebe
Artelove <arteevil.off@gmail.com>
2022-08-06 22:39:19 +0300

    58. Length of Last Word

---
+++ LeetCode/C#/1. Easy/58. Length of Last Word/58. Length of Last Word/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        string s = "dsfs d sdf sdf sdf sdfs dffff";
+        Console.WriteLine(solution.LengthOfLastWord(s));
+    }
+    public int LengthOfLastWord(string s)
+    {
+        int lastSpaceIndex = 0, i, count = 0 ;
+        for (i = 0; i < s.Length - 1; i++)
+            if (s[i] == ' ' && s[i + 1] != ' ') 
+                lastSpaceIndex = i+1;
+        i = lastSpaceIndex;
+        while (i < s.Length && s[i] != ' ')
+        {
+            count++;
+            i++;
+        }    
+        return count;
+    }
+}
