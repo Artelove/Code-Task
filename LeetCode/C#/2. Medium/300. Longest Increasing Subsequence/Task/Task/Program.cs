9c72b6da1e6e89917e1fa8312895cb896929550b
Artelove <arteevil.off@gmail.com>
2022-08-08 12:59:25 +0300

    300. Longest Increasing Subsequence

---
+++ LeetCode/C#/2. Medium/300. Longest Increasing Subsequence/Task/Task/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = new[] {2,15,3,7,8,6,18};
+        Console.WriteLine(solution.LengthOfLIS(nums));
+    }
+
+    public int LengthOfLIS(int[] nums) 
+    {
+        if(nums == null || nums.Count() == 0)
+            return 0;
+        var dp = new List<int>();
+        foreach(var num in nums)
+        {
+            if(!dp.Any() || dp[^1] < num)
+            {
+                dp.Add(num);
+            }
+            else
+            {
+                var index = dp.BinarySearch(num);
+                if(index < 0)
+                {
+                    index = ~index;
+                }
+                dp[index] = num;
+            }
+        }
+        return dp.Count();
+    }
+}
