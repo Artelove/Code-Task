aaee49ea862f7a2fb190e4118f432379fd580c3d
Aleksandr Karagichev <66765809+Artelove@users.noreply.github.com>
2023-02-27 08:59:01 +0300

    Update 217. Contains Duplicate.cs

--- LeetCode/C#/1. Easy/217. Contains Duplicate/217. Contains Duplicate.cs
+++ LeetCode/C#/1. Easy/217. Contains Duplicate/217. Contains Duplicate.cs
@@ -1,3 +1,7 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         Dictionaryint,int dict = new Dictionaryint,int();
