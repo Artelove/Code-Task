0ad7d3a39930e8aa6701fccff2b755f94fd4a645
EthicalSoftwareBot0 <arteevil.off@gmail.com>
2023-02-28 13:01:37 +0300

    Create 1. Two Sum(Dictionary).cs

---
+++ LeetCode/C#/1. Easy/1. Two Sum/1. Two Sum(Dictionary).cs
@@ -0,0 +1,17 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ */
+
+
+Dictionary<int,int> dictionary = new ();
+for (int i = 0; i < nums.Length; i++)
+{
+    if (dictionary.ContainsKey(target - nums[i]))
+        return new int[] { i, dictionary[target-nums[i]]};
+    else
+    {
+        dictionary.TryAdd(nums[i],i);
+    }
+}
