195c6d07d3ae8da4cbc2397d2efd3650895868b5
Aleksandr Karagichev <arteevil.off@gmail.com>
2023-10-31 10:27:30 +0300

    70. Climbing Stairs

---
+++ LeetCode/C#/1. Easy/70. Climbing Stairs/70. Climbing Stairs/70. Climbing Stairs/Program.cs
@@ -0,0 +1,26 @@
+﻿Solution solution = new Solution();
+Console.WriteLine(solution.ClimbStairs(5));
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        return DoStep(n, 0);
+    }
+
+    public int DoStep(int n, int current)
+    {
+        int result = 0;
+        if (current + 1 <= n)
+        {
+            result+=DoStep(n, current + 1);
+        }
+        if (current + 2 <= n)
+        {
+            result += DoStep(n, current + 2);
+        }
+        if (current == n)
+            result++;
+        return result;
+    }
+}
