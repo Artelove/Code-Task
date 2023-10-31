a2eb2510a042b71ba71ce039b87081be0814204e
Aleksandr Karagichev <arteevil.off@gmail.com>
2023-10-31 10:50:38 +0300

    83. Remove Duplicates from Sorted List

---
+++ LeetCode/C#/1. Easy/83. Remove Duplicates from Sorted List/83. Remove Duplicates from Sorted List/83. Remove Duplicates from Sorted List.cs
@@ -0,0 +1,43 @@
+﻿
+ 
+
+Console.WriteLine(value: new Solution().DeleteDuplicates(new ListNode(0)));
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head == null)
+            return head;
+        if (head.next == null)
+            return head;
+        ListNode previousValue = head;
+        ListNode current = head.next;
+        do
+        {
+            if (current.val == previousValue.val)
+            {
+                previousValue.next = null;
+                current = current.next;
+                continue;
+            }
+            else
+            {
+                if(previousValue.next == null)
+                    previousValue.next = current;
+                previousValue = previousValue.next;
+                current = current.next;
+            }
+        } while (current != null);
+        return head;
+    }
+}
